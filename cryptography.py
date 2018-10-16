f5c3fcab4676f24dbf152e1d8b9aea0d5c7b58f4
hackmeehan <42844639+hackmeehan@users.noreply.github.com>
2018-10-16 13:21:00 -0400

    Updated from Brython Server: 10/16/2018 1:21:00 PM

--- cryptography.py
+++ cryptography.py
@@ -9,36 +9,54 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
 A = input('Enter e to encrypt, d to decrypt, or q to quit: ')
 
 if A == 'e':
     message = input('Message: ')
     key = input('Key: ')
+    z = len(message)
+    f = len(key)
     a = zip(message, key)
-    for m in a:
-        if len(key) == len(message):
-            b = associations.find(m[0])
-            c = associations.find(m[1])
-            print(associations.find(b+c))
-        elif len(key) > len(message):
-            key = key[0:len(message)]
-            b = associations.find(m[0])
-            c = associations.find(m[1])
-            print(b+c)
-        else:
-            l = (len(message)/len(key))
-            key = key*l
-            b = associations.find(m[0])
-            c = associations.find(m[1])
-            print(b+c)
+    if f == z:
+        for l in a:
+            b = associations.find(l[0])
+            c = associations.find(l[1])
+            d = b+c
+            e = associations[d]
+            print(e)
+    if f < z:
+        e = z/f
+        import math
+        r = math.ceil(e)
+        key = r*key
+        for l in zip(message, key):
+            b = associations.find(l[0])
+            c = associations.find(l[1])
+            d = (b+c)
+            e = associations[d]
+            print(e)
+    if f > z:
+        for l in a:
+            b = associations.find(l[0])
+            c = associations.find(l[1])
+            d = (b+c)
+            e = associations[d]
+            print(e)
 elif A == 'd':
     message2 = input('Message: ')
     key2 = input('Key: ')
-    for j in message2:
-        print(associations[j])
+    for o in message2:
+        p = associations.find(o)
+    for u in key2:
+        t = associations.find(u)
+        h = p-t
+        print(h)
+        print(associations[h])
 elif A == 'q':
     print('Goodbye!')
 else:
     print('Did not understand, try again.')
+    
+# +KF;B(CH=NIZ}m;R\Dt
