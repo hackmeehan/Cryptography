af553765307f4c5506752f2acbb30785f2499b1a
hackmeehan <42844639+hackmeehan@users.noreply.github.com>
2018-10-23 12:25:10 -0400

    Updated from Brython Server: 10/23/2018 12:25:09 PM

--- cryptography.py
+++ cryptography.py
@@ -13,75 +13,74 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 
 A = input('Enter e to encrypt, d to decrypt, or q to quit: ')
 while A != 'q' and A != 'e' and A != 'd':
-    print('Did not understand, try again: ')
-    A = input('Enter e to encrypt, d to decrypt or q to quit ')
-if A == 'e':
-    message = input('Message: ')
-    key = input('Key: ')
-    z = len(message)
-    f = len(key)
-    a = zip(message, key)
-    if f == z:
-        for l in a:
-            b = associations.find(l[0])
-            c = associations.find(l[1])
-            d = b+c
-            e = str(associations[d])
-            print(e, end="")
-    if f < z:
-        e = z/f
-        import math
-        r = math.ceil(e)
-        key = r*key
-        for l in zip(message, key):
-            b = associations.find(l[0])
-            c = associations.find(l[1])
-            d = (b+c)
-            e = associations[d]
-            q = print(e, end="")
-    if f > z:
-        for l in a:
-            b = associations.find(l[0])
-            c = associations.find(l[1])
-            d = (b+c)
-            e = associations[d]
-            print(e, end="")
+    if A == 'e':
+        message = input('Message: ')
+        key = input('Key: ')
+        z = len(message)
+        f = len(key)
+        a = zip(message, key)
+        if f == z:
+            for l in a:
+                b = associations.find(l[0])
+                c = associations.find(l[1])
+                d = b+c
+                e = str(associations[d])
+                print(e, end="")
+        if f < z:
+            e = z/f
+            import math
+            r = math.ceil(e)
+            key = r*key
+            for l in zip(message, key):
+                b = associations.find(l[0])
+                c = associations.find(l[1])
+                d = (b+c)
+                e = associations[d]
+                q = print(e, end="")
+        if f > z:
+            for l in a:
+                b = associations.find(l[0])
+                c = associations.find(l[1])
+                d = (b+c)
+                e = associations[d]
+                print(e, end="")
+                
+    elif A == 'd':
+        message2 = input('Message: ')
+        key2 = input('Key: ')
+        z = len(message2)
+        f = len(key2)
+        a = zip(message2, key2)
+        if f == z:
+            for l in a:
+                b = associations.find(l[0])
+                c = associations.find(l[1])
+                d = b-c
+                e = str(associations[d])
+                print(e, end="")
+        if f < z:
+            e = z/f
+            import math
+            r = math.ceil(e)
+            key2 = r*key2
+            for l in zip(message2, key2):
+                b = associations.find(l[0])
+                c = associations.find(l[1])
+                d = (b-c)
+                e = associations[d]
+                q = print(e, end="")
+        if f > z:
+            for l in a:
+                b = associations.find(l[0])
+                c = associations.find(l[1])
+                d = (b-c)
+                e = associations[d]
+                print(e, end="")
             
-elif A == 'd':
-    message2 = input('Message: ')
-    key2 = input('Key: ')
-    z = len(message2)
-    f = len(key2)
-    a = zip(message2, key2)
-    if f == z:
-        for l in a:
-            b = associations.find(l[0])
-            c = associations.find(l[1])
-            d = b-c
-            e = str(associations[d])
-            print(e, end="")
-    if f < z:
-        e = z/f
-        import math
-        r = math.ceil(e)
-        key2 = r*key2
-        for l in zip(message2, key2):
-            b = associations.find(l[0])
-            c = associations.find(l[1])
-            d = (b-c)
-            e = associations[d]
-            q = print(e, end="")
-    if f > z:
-        for l in a:
-            b = associations.find(l[0])
-            c = associations.find(l[1])
-            d = (b-c)
-            e = associations[d]
-            print(e, end="")
-        
-elif A == 'q':
-    print('Goodbye!')
-else:
-    print('Did not understand, try again.')
-    
+    elif A == 'q':
+        print('Goodbye!')
+    else:
+        print('Did not understand, try again.')
+A = input('Enter e to encrypt, d to decrypt or q to quit ')
+
 # +KF;B(CH=NIZ}m;R\Dt
